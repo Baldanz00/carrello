e73229b183926a151e6fb0635be90145a92967c4
Baldanz00 <gioia.travaglini18@gmail.com>
2025-12-01 15:17:18 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+/*Un carrello è una collezione di beni, ciascuno presente in unaq determinata quantità, che rappresentano gli
+* acquisti di un utente su un sito di commercio elettronico.
+*
+* Un carrello supporta il seguente insieme di operazioni:
+* - aggiungi N B, che aggiunge N volte un bene B dal carrello;
+* - togli N B, che toglie N volte il bene B dal carrello (togliere un bene assente dal carrello non ha effetto,
+* mentre togliere un bene presente, ma per un valore di volte maggiore della sua quantità nel carrello,
+* lo elimina dal carrello;
+* - quanti B che emette il numero corrispondente alla quantità del bene B presente nel carrello (o 0 se il bene
+* non è presente);
+* - vuoto che emette SI o NO a seconda che il carrello sia, o meno, vuoto;
+*
+* Dove N è un numero intero positivo e B una stringa (contenente anche spazi).
+* Ad esempio, a partire da un carrello vuoto, l'elenco di operazioni
+*
+* vuoto
+* aggiungi 3 cuffie
+* aggiungi 1 lettore mp3
+* togli una cuffia
+* quanti cuffia
+*  contiene lettore mp3
+* vuoto
+*
+* emette:
+* si
+* 2
+* si
+* no*/
+}
